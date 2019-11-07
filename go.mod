f574b54218bae7f330a659e578864d5e4a5c9137
Mateusz Warzyński <mathew.warzynski@gmail.com>
2019-11-08 00:29:19 +0100

    Add initial main and dependencies

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mwarzynski/loodse-k8s-node-label-controller
+
+go 1.13
+
+require (
+	k8s.io/client-go v0.0.0-20191016111102-bec269661e48
+	k8s.io/klog v1.0.0
+)
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog"
+)
+
+var (
+	kubeconfig string
+	masterURL  string
+)
+
+func main() {
+	klog.InitFlags(nil)
+	flag.Parse()
+
+	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+	if err != nil {
+		klog.Fatalf("Error building kubeconfig: %s", err.Error())
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+	_ = kubeClient
+
+	println("Hello, I am your Kubernetes Controller")
+}
+
+func init() {
+	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+}
